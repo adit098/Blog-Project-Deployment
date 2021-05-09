43a80fc3057f49a6daf639b15ca2e37aeb80da54
adit098 <adityakumargupta151@gmail.com>
2021-05-09 19:39:00 +0530

    allowed hosts edit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l$^5e(-d-7=50nf+0i_a77pc%%@l$r0=_t*bgv0hk@!ek)@ib='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adit098.pythonanywhere.com']
 
 
 # Application definition
